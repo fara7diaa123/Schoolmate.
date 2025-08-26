f8990c86318dad68fa120989726ff49643da793a
fara7diaa123 <fara7asdi@gmail.com>
2025-08-26 21:55:23 +0300

    Delete attendance.js

--- attendance.js
+++
@@ -1,9 +0,0 @@
-function markPresent(btn) {
-  btn.parentNode.style.backgroundColor = "#4CAF50";  
-  btn.parentNode.innerHTML = "✔ حضور";  
-}
-
-function markAbsent(btn) {
-  btn.parentNode.style.backgroundColor = "#F44336";  
-  btn.parentNode.innerHTML = "✘ غياب";  
-}
